377876301c631949919538dbc9a9cc7e3ff6c08b
Mohammad Aslam <aslampaasa420@gmail.com>
2026-04-14 20:39:14 +0530

    Set Database Access

--- server/agent.ts
+++ server/agent.ts
@@ -1,26 +1,37 @@
 import { ChatOpenAI } from '@langchain/openai';
 import { MessagesAnnotation } from '@langchain/langgraph';
 import { initDB } from './db.ts';
+import { initTools } from './tools.ts';
+import { ToolNode } from '@langchain/langgraph/prebuilt';
 
 
-/* 2. Init Database (dbPath) */
+/* 2. Init Database (dbPath) + Set DB Access to Tools*/
 const database = initDB("./expenses.db");
-
+const tools = initTools(database);
 
 /* 1. Initialize the LLM Model */
 const llm = new ChatOpenAI({
-    model: 'gpt-5.1-mini',
+    model: 'gpt-4.1-mini',
     temperature: 0,
 });
 
+/**
+ * 3. Prepare Tool Node
+*/
+const toolNode = new ToolNode(tools);
 
-/* 3. Prepare Tool Node */
+/* 4. Prepare Call Model Node + Add Mmmory + Bind Tools*/
+async function callModel(state: typeof MessagesAnnotation.State) {
+    const llmWithTools = llm.bindTools(tools);
+    const response = await llmWithTools.invoke([
+        {
+            role: 'system',
+            content: `You are a helpful tracking assistant. Current datetime: ${new Date().toISOString()}.
+                      Call add_expense tool to add the expense to database.
+                    `
+        },
+        ...state.messages
+    ])
 
-
-/* 4. Prepare Call Model Node */
-async function callModel(state: typeof MessagesAnnotation) {
-    return state;
+    return { messages: [response] };
 }
-
-
-/* 3.  */
--- server/tools.ts
+++ server/tools.ts
@@ -1,19 +1,28 @@
-/* Prepare Tool Node: */
-
+import { DatabaseSync } from 'node:sqlite';
 import { tool } from 'langchain';
 import * as z from 'zod';
 
-export const addExpense = tool(
-    ({title, amount}) => {
-        console.log({title, amount});
-        return JSON.stringify({status: "success"});
-    },
-    {
-        name: 'add_expense',
-        description: 'Add the given expense to database',
-        schema: z.object({
-            title: z.string().describe('The expense title'),
-            amount: z.number().describe('The amount spent')
-        })
-    }
-)
+/* Setup Database Access + Prepare Tools */ 
+export function initTools(database: DatabaseSync) {
+    
+    /* Tool Node-1: Add Expense */ 
+    const addExpense = tool(
+        ({title, amount}) => {
+            console.log({title, amount});
+            return JSON.stringify({status: "success"});
+        },
+        {
+            name: 'add_expense',
+            description: 'Add the given expense to database',
+            schema: z.object({
+                title: z.string().describe('The expense title'),
+                amount: z.number().describe('The amount spent')
+            })
+        }
+    )
+
+    /* return tools */ 
+    return [addExpense]
+}
+
+
