2486eb9f63daf2e3b2182b1763770fc3db975b57
Mohammad Aslam <aslampaasa420@gmail.com>
2026-04-14 21:11:29 +0530

    Compile Graph

--- server/agent.ts
+++ server/agent.ts
@@ -1,11 +1,12 @@
 import { ChatOpenAI } from '@langchain/openai';
-import { MessagesAnnotation } from '@langchain/langgraph';
+import { MemorySaver, MessagesAnnotation, StateGraph } from '@langchain/langgraph';
 import { initDB } from './db.ts';
 import { initTools } from './tools.ts';
 import { ToolNode } from '@langchain/langgraph/prebuilt';
+import { AIMessage } from 'langchain';
 
 
-/* 2. Init Database (dbPath) + Set DB Access to Tools*/
+/* 2. Init Database (dbPath) */
 const database = initDB("./expenses.db");
 const tools = initTools(database);
 
@@ -16,11 +17,11 @@ const llm = new ChatOpenAI({
 });
 
 /**
- * 3. Prepare Tool Node
+ * 3. Prepare Tool Node + Set DB Access to Tools
 */
 const toolNode = new ToolNode(tools);
 
-/* 4. Prepare Call Model Node + Add Mmmory + Bind Tools*/
+/* 4. Prepare Call Model Node + Store Messages + Bind Tools*/
 async function callModel(state: typeof MessagesAnnotation.State) {
     const llmWithTools = llm.bindTools(tools);
     const response = await llmWithTools.invoke([
@@ -35,3 +36,48 @@ async function callModel(state: typeof MessagesAnnotation.State) {
 
     return { messages: [response] };
 }
+
+/**
+ * Conditional Edge
+*/
+function shouldContinue(state: typeof MessagesAnnotation.State) {
+    const messages = state.messages;
+    const lastMessage = messages.at(-1) as AIMessage;
+
+    if(lastMessage.tool_calls?.length) {
+        return 'tools';
+    }
+    return '__end__';
+}
+
+
+/**
+ * 5. Compile Graph + Add Memory
+*/
+const graph = new StateGraph(MessagesAnnotation)
+    .addNode('callModel', callModel)
+    .addNode('tools', toolNode)
+    .addEdge('__start__', 'callModel')
+    .addConditionalEdges('callModel', shouldContinue, {
+        '__end__':'__end__', 
+        'tools':'tools'
+    })
+
+const agent = graph.compile({ checkpointer: new MemorySaver() });
+
+
+/* 6. Invoke AI Agent: node --env-file=.env agent.ts */
+async function main() {
+    const response = await agent.invoke({
+        messages: [
+            {
+                role: 'user',
+                content: 'I just bought a laptop for 80,000 inr'
+            }
+        ]
+    }, { configurable: { thread_id: '1'} })
+
+    console.log(JSON.stringify(response, null, 2));
+}
+
+main()
--- server/tools.ts
+++ server/tools.ts
@@ -5,10 +5,15 @@ import * as z from 'zod';
 /* Setup Database Access + Prepare Tools */ 
 export function initTools(database: DatabaseSync) {
     
-    /* Tool Node-1: Add Expense */ 
+    /* Tool Node-1: Add Expense to DB */ 
     const addExpense = tool(
         ({title, amount}) => {
             console.log({title, amount});
+            
+            const date = new Date().toISOString().split('T')[0];
+            const stmt = database.prepare(`INSERT INTO expenses (title, amount, date) VALUES(?, ?, ?)`);
+            stmt.run(title, amount, date);
+
             return JSON.stringify({status: "success"});
         },
         {
