c0987faae6e300cb6c4d6d81c5bba6a329aeb2ac
Mohammad Aslam <aslampaasa420@gmail.com>
2026-04-15 20:47:41 +0530

    Setup Tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Welcome to Expense Tracker</h1>
+      <h1 className="text-3xl font-bold underline text-purple-900">Welcome to Expense Tracker</h1>
     </>
   );
 }
