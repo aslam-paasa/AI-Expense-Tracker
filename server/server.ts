bd01cfb7265f187b61d380b2683c578853764a02
Mohammad Aslam <aslampaasa420@gmail.com>
2026-04-18 12:28:40 +0530

    Setup SSE at Server Side

--- server/server.ts
+++ server/server.ts
@@ -11,6 +11,24 @@ app.get('/', (req, res) => {
     res.json({ message: 'OK' })
 })
 
+/* SSE: Server Sent Event Protocol */
+app.get('/chat', (req, res) => {
+
+    /* 1. add special header */
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream'
+    });
+
+    /* 2. Send data in special format */
+    setInterval(() => {
+        res.write("event: cgPing/n")    // q. Event Key
+        res.write('data: Happy Coding') // b. Sending data 
+        res.write("/n/n")               // c. 2 New Line
+    }, 1000);
+
+    res.json({});
+})
+
 
 const PORT = process.env.PORT ||4100;
 app.listen(PORT, () => {
